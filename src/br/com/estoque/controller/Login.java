0ce135401e1a67b132a0f10a7c7b14878285458e
joseluiz <joseluiz@localhost>
2019-09-01 17:53:29 -0300

    alterações diversas

--- src/br/com/estoque/controller/Login.java
+++ src/br/com/estoque/controller/Login.java
@@ -17,47 +17,48 @@ import br.com.estoque.model.ModelUsuario;
  */
 @WebServlet("/Login")
 public class Login extends HttpServlet {
-	ModelSessaoUsuario modelSessaoUsuario = new ModelSessaoUsuario();
-	ModelUsuario modelUsuario = new ModelUsuario();
-	ControllerUsuario controllerUsuario = new ControllerUsuario();
 
-	private static final long serialVersionUID = 1L;
+    ModelSessaoUsuario modelSessaoUsuario = new ModelSessaoUsuario();
+    ModelUsuario modelUsuario = new ModelUsuario();
+    ControllerUsuario controllerUsuario = new ControllerUsuario();
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public Login() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
-		doGet(request, response);
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
 
-		modelUsuario.setUsuLogin(request.getParameter("login"));
-		modelUsuario.setUsuSenha(request.getParameter("senha"));
+        doGet(request, response);
 
-		try {
-			if (controllerUsuario.getValidarUsuarioController(modelUsuario)) {
-				modelUsuario = controllerUsuario.getUsuarioController(request.getParameter("login"));
-				ModelSessaoUsuario.codigoUsuario = modelUsuario.getIdUsuario();
-				ModelSessaoUsuario.nomeUsuario = modelUsuario.getUsuNome();
-				ModelSessaoUsuario.loginUsuario = modelUsuario.getUsuSenha();
-				request.getRequestDispatcher("mensagem.jsp").forward(request, response);
-			} else {
-				request.getRequestDispatcher("erro.jsp").forward(request, response);
-			}
-		} catch (Exception e) {
-			out.println();
-		}
+        modelUsuario.setUsuLogin(request.getParameter("login"));
+        modelUsuario.setUsuSenha(request.getParameter("senha"));
 
-	}
+        try {
+            if (controllerUsuario.getValidarUsuarioController(modelUsuario)) {
+                modelUsuario = controllerUsuario.getUsuarioController(request.getParameter("login"));
+                ModelSessaoUsuario.codigoUsuario = modelUsuario.getIdUsuario();
+                ModelSessaoUsuario.nomeUsuario = modelUsuario.getUsuNome();
+                ModelSessaoUsuario.loginUsuario = modelUsuario.getUsuSenha();
+                request.getRequestDispatcher("mensagem.jsp").forward(request, response);
+            } else {
+                request.getRequestDispatcher("erro.jsp").forward(request, response);
+            }
+        } catch (Exception e) {
+            out.println();
+        }
+
+    }
 
 }
